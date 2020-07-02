9d25288b344d74381ce1e72e060af2982d54f3c3
Erix Mamani Villacresis <erixmv022@gmail.com>
2020-07-02 12:01:12 +0200

    Added extractData in Auth.helpers

--- src/api/helpers/Auth.helper.ts
+++ src/api/helpers/Auth.helper.ts
@@ -1,4 +1,4 @@
-import * as jwt from "jsonwebtoken"
+import * as jwt from "jsonwebtoken";
 
 //* Json Web Token Class
 /**
@@ -9,22 +9,35 @@ export default class AuthHelper {
     // Method: Creates a Json web token that contains the user id
     public async jwtSign(_id: Object): Promise<any> {
 
-        const id = JSON.stringify(_id)
+        const id = JSON.stringify(_id);
 
         const jwtToken = jwt.sign({
             data: id
         }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-        return jwtToken
+        return jwtToken;
     }
 
+    // Method: Checks if the token is valid
     public async compareJwt(token: String): Promise<any> {
-
         try {
-            const test = await jwt.verify(token, process.env.JWT_SECRET);
+            await jwt.verify(token, process.env.JWT_SECRET);
             return true;
         } catch (error) {
             return false;
+        }
+    }
+
+    //! Method: Extract the data from payload
+    public async extractData(authorization: String): Promise<any> {
+
+        const [ token ]  = authorization.split(' ').slice(-1);
+
+        try {
+            const { data } = await jwt.verify(token, process.env.JWT_SECRET);
+            return data;
+        } catch (error) {
+            console.log(error);
         }
     }
 
